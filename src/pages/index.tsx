15140fde79cd756f4736a87e8009509f37923963
MShafiMS <shahedali734@gmail.com>
2024-07-13 00:08:22 +0600

    fix: push to drawing page from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import { Layout } from '@enums';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const { push } = useRouter();
+  useEffect(() => {
+    push('/drawings');
+  }, []);
   return <div>Hello world</div>;
 };
 
