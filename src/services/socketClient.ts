63059292bd374e5a2b6c7f255d42e6371361d9b1
MShafiMS <shahedali734@gmail.com>
2024-07-12 23:24:26 +0600

    fix: removed some packages

--- src/services/socketClient.ts
+++ src/services/socketClient.ts
@@ -1,17 +1,17 @@
-import { io, Socket } from 'socket.io-client';
+// import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('http://localhost:5000'); // Adjust the URL if your server is hosted elsewhere
+// const socket: Socket = io('http://localhost:5000'); // Adjust the URL if your server is hosted elsewhere
 
-export const joinRoom = (roomId: string): void => {
-  socket.emit('joinRoom', roomId);
-};
+// export const joinRoom = (roomId: string): void => {
+//   socket.emit('joinRoom', roomId);
+// };
 
-export const sendSketchData = (roomId: string, sketch: any): void => {
-  socket.emit('sketchData', { roomId, sketch });
-};
+// export const sendSketchData = (roomId: string, sketch: any): void => {
+//   socket.emit('sketchData', { roomId, sketch });
+// };
 
-export const receiveSketchData = (callback: (sketch: any) => void): void => {
-  socket.on('receiveSketchData', (sketch: any) => {
-    callback(sketch);
-  });
-};
+// export const receiveSketchData = (callback: (sketch: any) => void): void => {
+//   socket.on('receiveSketchData', (sketch: any) => {
+//     callback(sketch);
+//   });
+// };
